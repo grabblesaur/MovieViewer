4c6229499a87a603f1cdadb7f19ba857e142266d
grabblesaur <ghjcnjnfr219532>
2016-10-23 17:17:05 +0800

    example of retain fragment

--- app/src/main/java/com/bbayar/movieviewer/view/MainActivity.java
+++ app/src/main/java/com/bbayar/movieviewer/view/MainActivity.java
@@ -10,98 +10,38 @@ import android.view.MenuItem;
 import android.view.View;
 
 import com.bbayar.movieviewer.R;
-import com.bbayar.movieviewer.model.Result;
-import com.bbayar.movieviewer.model.TvResponse;
-import com.bbayar.movieviewer.model.rest.ApiClient;
-import com.bbayar.movieviewer.model.rest.ApiInterface;
-
-import org.parceler.Parcels;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
-    private static final String RESULT_LIST_KEY = "list";
-    public static String API_KEY = "21f91637045fc30ac59759b75acc9ca0";
+    public static String GLOBAL_TAG = "global";
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
 
-    private List<Result> resultList;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d(TAG, "onCreate() called with: savedInstanceState = [" + savedInstanceState + "]");
-        Log.d(TAG, "onCreate: resultList = " + resultList);
+        Log.d(GLOBAL_TAG, "MainActivity onCreate: ");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
-
-        if (savedInstanceState == null
-                || !savedInstanceState.containsKey(RESULT_LIST_KEY)) {
-            resultList = new ArrayList<>();
-            loadData();
-        } else {
-            resultList = Parcels.unwrap(savedInstanceState.getParcelable(RESULT_LIST_KEY));
-        }
-
-        Log.d(TAG, "resultList = " + resultList);
-
-        showMainFragment();
-
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        if (resultList != null) {
-            outState.putParcelable(RESULT_LIST_KEY, Parcels.wrap(resultList));
+        if (savedInstanceState == null) {
+            showMainFragment();
         }
     }
 
     private void showMainFragment() {
-        Log.d(TAG, "showMainFragment() called");
+        Log.d(GLOBAL_TAG, "MainActivity showMainFragment() called");
         getSupportFragmentManager()
                 .beginTransaction()
-                .add(R.id.fragment_container,
-                        MainFragment.newInstance(resultList),
+                .replace(R.id.fragment_container,
+                        MainFragment.newInstance(),
                         MainFragment.class.getSimpleName())
                 .commit();
-    }
-
-    private void loadData() {
-        Log.d(TAG, "loadData: started");
-        ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
-        Call<TvResponse> call = apiService.getTvResponse(API_KEY);
-        call.enqueue(new Callback<TvResponse>() {
-            @Override
-            public void onResponse(Call<TvResponse> call, Response<TvResponse> response) {
-                resultList.addAll(response.body().getResults());
-                Log.d(TAG, "onResponse: resultList = " + resultList);
-
-                //// TODO: 19.10.2016 something with notify, that some items are added
-                MainFragment fragment = (MainFragment) getSupportFragmentManager()
-                        .findFragmentByTag(MainFragment.class.getSimpleName());
-                fragment.getAdapter().notifyItemInserted(0);
-                fragment.getAdapter().notifyItemRangeChanged(0, resultList.size());
-            }
-
-            @Override
-            public void onFailure(Call<TvResponse> call, Throwable t) {
-                Log.d(TAG, "onFailure: " + t.toString());
-                t.printStackTrace();
-            }
-        });
     }
 
     @OnClick(R.id.fab) public void onFabClick(View view) {
--- app/src/main/java/com/bbayar/movieviewer/view/MainFragment.java
+++ app/src/main/java/com/bbayar/movieviewer/view/MainFragment.java
@@ -1,7 +1,6 @@
 package com.bbayar.movieviewer.view;
 
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -13,20 +12,26 @@ import android.view.ViewGroup;
 
 import com.bbayar.movieviewer.R;
 import com.bbayar.movieviewer.model.Result;
+import com.bbayar.movieviewer.model.TvResponse;
+import com.bbayar.movieviewer.model.rest.ApiClient;
+import com.bbayar.movieviewer.model.rest.ApiInterface;
 import com.bbayar.movieviewer.view.adapter.MoviesAdapter;
 
-import org.parceler.Parcels;
-
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static com.bbayar.movieviewer.view.MainActivity.GLOBAL_TAG;
 
 public class MainFragment extends Fragment {
 
-    private static final String TAG = "MainFragment";
-    private static String RESULT_LIST_KEY = "result list";
+    private static String API_KEY = "21f91637045fc30ac59759b75acc9ca0";
 
     @BindView(R.id.recyclerview)
     RecyclerView recyclerView;
@@ -36,34 +41,64 @@ public class MainFragment extends Fragment {
     private List<Result> resultList;
     private MoviesAdapter moviesAdapter;
 
-    public static MainFragment newInstance(List<Result> list) {
-        Log.d(TAG, "newInstance() called with: list = [" + list + "]");
-        MainFragment fragment = new MainFragment();
-        Parcelable p = Parcels.wrap(list);
-        Bundle bundle = new Bundle();
-        bundle.putParcelable(RESULT_LIST_KEY, p);
-        fragment.setArguments(bundle);
-        return fragment;
+    public static MainFragment newInstance() {
+        Log.d(GLOBAL_TAG, "MainFragment newInstance(): ");
+        return new MainFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        Log.d(GLOBAL_TAG, "MainFragment onCreate: ");
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        Log.d(TAG, "onCreateView() called with: inflater = [" + inflater + "], container = [" + container + "], savedInstanceState = [" + savedInstanceState + "]");
+        Log.d(GLOBAL_TAG, "MainFragment onCreateView():");
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        Log.d(TAG, "onViewCreated() called with: view = [" + view + "], savedInstanceState = [" + savedInstanceState + "]");
+        Log.d(GLOBAL_TAG, "MainFragment onViewCreated():");
         super.onViewCreated(view, savedInstanceState);
         unbinder = ButterKnife.bind(this, view);
-        resultList = Parcels.unwrap(getArguments().getParcelable(RESULT_LIST_KEY));
-        Log.d(TAG, "onViewCreated: resultList: " + resultList);
+
+        if (resultList == null) {
+            resultList = new ArrayList<>();
+            loadData();
+        }
         setUpRecyclerView();
     }
 
+    private void loadData() {
+        Log.d(GLOBAL_TAG, "MainFragment loadData: started");
+        ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
+        Call<TvResponse> call = apiService.getTvResponse(API_KEY);
+        call.enqueue(new Callback<TvResponse>() {
+            @Override
+            public void onResponse(Call<TvResponse> call, Response<TvResponse> response) {
+                resultList.addAll(response.body().getResults());
+                Log.d(GLOBAL_TAG, "MainFragment onResponse: resultList = " + resultList);
+
+                //// TODO: 19.10.2016 something with notify, that some items are added
+                moviesAdapter.notifyItemInserted(0);
+                moviesAdapter.notifyItemRangeChanged(0, resultList.size());
+            }
+
+            @Override
+            public void onFailure(Call<TvResponse> call, Throwable t) {
+                Log.d(GLOBAL_TAG, "MainFragment onFailure: " + t.toString());
+                t.printStackTrace();
+            }
+        });
+        Log.d(GLOBAL_TAG, "MainFragment loadData: ended");
+    }
+
     private void setUpRecyclerView() {
+        Log.d(GLOBAL_TAG, "MainFragment setUpRecyclerView: ");
         moviesAdapter = new MoviesAdapter(resultList);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(moviesAdapter);
@@ -71,12 +106,8 @@ public class MainFragment extends Fragment {
 
     @Override
     public void onDestroy() {
-        Log.d(TAG, "onDestroy() called");
+        Log.d(GLOBAL_TAG, "MainFragment onDestroy() called");
         super.onDestroy();
         unbinder.unbind();
-    }
-
-    public MoviesAdapter getAdapter() {
-        return moviesAdapter;
     }
 }
